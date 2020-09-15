a6445d261cb04bc8e17ecbe1dec7f8db43d444c6
ndh13 <ndh13@DESKTOP-EPEG9JC>
2020-09-15 15:21:12 +0900

    project regist

---
+++ oh_erp/src/main/java/com/kh/oherp/controller/ProjectController.java
@@ -0,0 +1,37 @@
+package com.kh.oherp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.oherp.entity.ProjectDto;
+import com.kh.oherp.repository.ProjectDao;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+	@GetMapping("/regist")
+	public String regist() {
+		return "project/regist";
+	}
+	
+	@Autowired
+	private ProjectDao projectDao;
+	@PostMapping("/regist")
+	public String regist(
+			@ModelAttribute ProjectDto projectDto
+			) {
+		boolean result =projectDao.regist(projectDto);
+		
+		if(result) {
+			return "redirect:list";
+		} else {
+			return "redirect:regist?error";
+		}
+	}
+	
+}
