6497e6c7a40468731853943519201248fae45877
roh-dh" <ndh13399@naver.com>
2020-09-17 16:14:26 +0900

    비밀번호 암호화등록, 암호화된 비밀번호 로그인 및 세션부여

--- oh_erp/src/main/java/com/kh/oherp/controller/MemberController.java
+++ oh_erp/src/main/java/com/kh/oherp/controller/MemberController.java
@@ -1,6 +1,12 @@
 package com.kh.oherp.controller;
 
+import java.beans.Encoder;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +29,8 @@ public class MemberController {
 	private DepartmentDao departmentDao;
 	@Autowired
 	private MemberDao memberDao;
+	
+
 	
 	@GetMapping("/regist")
 	public String regist(Model model) {
@@ -49,4 +57,40 @@ public class MemberController {
 		memberDao.list(model);
 		return "member/list";
 	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "member/login";
+	}
+	
+	@Autowired 
+	private SqlSession sqlSession;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@PostMapping("/login")
+	public String login(
+			@ModelAttribute MemberDto memberDto,
+			HttpSession session) {
+//		[1] DB에서 해당 회원의 정보를 모두 불러온다
+		MemberDto find = sqlSession.selectOne(
+						"member.getMemberCode", memberDto.getMember_code());
+		if(find != null) {//아이디가 있으면
+//			[2] 아이디가 있을 경우 비밀번호 비교를 수행한다(encoder 사용)
+//			encoder.matches(입력 PW, DB PW) --> boolean
+			boolean pass = encoder.matches(
+					memberDto.getMember_pw(), find.getMember_pw());
+			if(pass) {//비밀번호 일치
+//				[3] 비밀번호도 맞을 경우 메인 페이지로 이동 및 세션 작업
+				session.setAttribute("userinfo", find);
+				return "redirect:/";
+			}
+		}
+
+		//그 외의 모든 경우는 오류로 간주
+		return "redirect:login?error";
+	
+	}
+	
 }
